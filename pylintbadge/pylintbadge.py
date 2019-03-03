1affb4a4c0382f274481e076551c971a338bdc5e
Bastian Stender <basti@randomprojects.de>
2019-03-03 19:50:55 +0100

    pylintbadge: add support for multiple files, just like in pylint
    Signed-off-by: Bastian Stender <basti@randomprojects.de>

--- pylintbadge/pylintbadge.py
+++ pylintbadge/pylintbadge.py
@@ -33,11 +33,11 @@ def main():
     from pylint.lint import Run
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_to_lint")
+    parser.add_argument("files_to_lint", nargs="+")
     args = parser.parse_args()
-    file_to_lint = args.file_to_lint
+    files_to_lint = args.files_to_lint
 
-    score = round(Run(file_to_lint, do_exit=False).linter.stats['global_note'], 2)
+    score = round(Run(files_to_lint, do_exit=False).linter.stats['global_note'], 2)
 
     template = '<svg xmlns="http://www.w3.org/2000/svg" width="85" height="20"><linearGradient id="a" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><rect rx="3" width="85" height="20" fill="#555"/><rect rx="3" x="50" width="35" height="20" fill="{color}"/><path fill="{color}" d="M50 0h4v20h-4z"/><rect rx="3" width="85" height="20" fill="url(#a)"/><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="25" y="15" fill="#010101" fill-opacity=".3">pylint</text><text x="25" y="14">pylint</text><text x="67" y="15" fill="#010101" fill-opacity=".3">{score}</text><text x="67" y="14">{score}</text></g></svg>'
 
